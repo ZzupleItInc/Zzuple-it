213a7137e5a454bbf558295dfe2f5effe3283b33
srb2thepast <alexhazoume@gmail.com>
2018-11-21 21:00:09 -0500

    New Scripts Added
    Because we had to update the old ones and we needed a way to put them in the folder, we reuploaded the files so that they are all in the same folder.

---
+++ Scripts/Camera_Signal_1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Signal_1 : MonoBehaviour {
+
+
+
+
+
+	public static int Move = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+		GetComponent<Camera_Move_1>().enabled = false;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Move == 1) {
+			GetComponent<Camera_Move_1> ().enabled = true;
+
+
+				
+		} 
+
+	}
+}
