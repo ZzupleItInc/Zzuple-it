cf5bfe0799bbd1a760fbdd18c24e6d7ac01caaf7
srb2thepast <alexhazoume@gmail.com>
2018-11-21 20:56:59 -0500

    Add files via upload

---
+++ Scripts/Camera_Move_1.cs
@@ -0,0 +1,20 @@
+﻿// Smooth towards the target
+
+using UnityEngine;
+using System.Collections;
+
+public class Camera_Move_1 : MonoBehaviour
+{
+	public Transform target;
+	public float smoothTime = 0.3F;
+	private Vector3 velocity = Vector3.zero;
+
+	void Update()
+	{
+		// Define a target position above and behind the target transform
+		Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+
+		// Smoothly move the camera towards that target position
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
---
+++ Scripts/Key_Appear_Open.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_Appear_Open : MonoBehaviour {
+
+
+	public int Appear = 0;
+	public int Equipped = 0;
+		
+
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<SpriteRenderer> ().enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Appear != 0)
+			GetComponent<SpriteRenderer> ().enabled = false;
+
+		{
+
+		
+
+		}
+		
+	}
+}
